093e96a144a49e07c598f11791f55476da0caa10
Alexander Sigler <alexander@siglerdev.us>
2019-03-17 02:06:41 -0700

    Initial Push

---
+++ cmake-build-debug/CMakeFiles/CPSC351_Project1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPSC351_Project1.dir/main.cpp.o"
+  "CMakeFiles/CPSC351_Project1.dir/recv.cpp.o"
+  "CMakeFiles/CPSC351_Project1.dir/sender.cpp.o"
+  "CPSC351_Project1.pdb"
+  "CPSC351_Project1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPSC351_Project1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
